57880678b4d8d77b8bb88e516df1d336ef390471
fabiolajsm <suarezfabiola17@gmail.com>
2023-10-16 10:59:34 -0300

    Se usa manejador de archivos en pizzaConsultar

--- PizzaConsultar.php
+++ PizzaConsultar.php
@@ -1,21 +1,17 @@
 <?php
+include 'ManejadorArchivos.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sabor = $_POST['sabor'];
     $tipo = $_POST['tipo'];
 
-    $jsonFile = 'Pizza.json';
-    $pizzaData = [];
-
-    if (file_exists($jsonFile)) {
-        $jsonData = file_get_contents($jsonFile);
-        $pizzaData = json_decode($jsonData, true);
-
-        if ($pizzaData === null) {
-            echo 'Error: el archivo JSON no es válido.';
-            return;
-        }
+    $archivoJson = 'Pizza.json';
+    $manejadorArchivos = new ManejadorArchivos($archivoJson);
+    $pizzaData = $manejadorArchivos->leer();
+    if (empty($pizzaData)) {
+        echo 'Error: el archivo JSON no es válido.';
+        return;
     }
-
     $pizzaExists = false;
 
     foreach ($pizzaData as $pizza) {
